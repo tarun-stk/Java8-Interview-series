6ebbbc6caba0b8be81c77a7a84c47ff153073e61
tarun-stk <tarunstk8897@gmail.com>
2023-07-25 17:03:12 +0530

    Added concat two streams

---
+++ src/com/java8/exercises/FindMaxNumberInAIntStream.java
@@ -0,0 +1,21 @@
+package com.java8.exercises;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindMaxNumberInAIntStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers = new ArrayList<>(Arrays.asList(234, 234, 23, 234, 234, 354, 45, 623, 42, 2, 654, 846846,
+				846, 4, 5432, 168, 432, 16, 84, 321, 684));
+		
+		System.out.println(numbers.stream().max((a, b) -> b-a).get());
+//		other way
+		System.out.println(numbers.stream().min((a, b) -> a-b).get());
+
+
+	}
+
+}
