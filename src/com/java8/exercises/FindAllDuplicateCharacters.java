41e4fb9b6126e2c0a521c0e903abcb9072767022
tarun-stk <tarunstk8897@gmail.com>
2023-07-31 20:01:30 +0530

    added find first non repetitive char in string

--- src/com/java8/exercises/FindAllDuplicateCharacters.java
+++ src/com/java8/exercises/FindAllDuplicateCharacters.java
@@ -2,19 +2,34 @@ package com.java8.exercises;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FindAllDuplicateCharacters {
 
 	public static void main(String[] args) {
 		List<Integer> numbers = new ArrayList<>(Arrays.asList(234, 234, 23, 234, 234, 354, 45, 623, 42, 2, 654, 846846,
 				846, 4, 5432, 168, 432, 16, 84, 321, 684, 2, 45));
-		
-		Set<Integer> dups = new HashSet<>();
-		numbers.stream().filter(x -> !dups.add(x)).forEach(System.out::println);;
 
+		numbers.stream()
+			.collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(x -> x.getValue() > 1)
+			.map(x -> x.getKey())
+			.forEach(System.out::println);
+		
+		String str = "Java8 is fun and amazing";
+
+		str.codePoints().mapToObj(x -> (char)x)
+			.collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(x -> x.getValue() > 1)
+			.map(x -> x.getKey())
+			.forEach(System.out::println);
+		;
+		
 	}
 
 }
---
+++ src/com/java8/exercises/FindFirstNonRepetitiveCharacterInAString.java
@@ -0,0 +1,24 @@
+package com.java8.exercises;
+
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindFirstNonRepetitiveCharacterInAString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "java is a object oriented programming language";
+		char ans = str.codePoints().mapToObj(x -> (char)x)
+			.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(x -> x.getValue() == 1)
+			.findFirst()
+			.map(x -> x.getKey())
+			.get();
+		
+		System.out.println("Ans: " + ans);
+	}
+
+}
