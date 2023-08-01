7be7aace888fd6f7f85cc4189edd7f0a50daa34c
tarun-stk <tarunstk8897@gmail.com>
2023-08-01 10:13:35 +0530

    added find first repetitive char in string

---
+++ src/com/java8/exercises/FindFirstRepetitiveCharacterInAString.java
@@ -0,0 +1,23 @@
+package com.java8.exercises;
+
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindFirstRepetitiveCharacterInAString {
+
+	public static void main(String[] args) {
+		String str = "java is a object oriented programming language";
+		char ans = str.codePoints().mapToObj(x -> (char)x)
+			.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(x -> x.getValue() > 1)
+			.findFirst()
+			.map(x -> x.getKey())
+			.get();
+		
+		System.out.println("Ans: " + ans); //a
+	}
+
+}
