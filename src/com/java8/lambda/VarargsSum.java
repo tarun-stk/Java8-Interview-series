5867adfc96a1cf20724fe805c214d372614727de
tarun-stk <tarunstk8897@gmail.com>
2024-05-23 16:34:20 +0530

    Updated some questions

---
+++ src/com/java8/lambda/VarargsSum.java
@@ -0,0 +1,7 @@
+package com.java8.lambda;
+
+@FunctionalInterface
+public interface VarargsSum {
+
+    int add(int ... x);
+}
--- src/com/java8/operations/Java8Operations.java
+++ src/com/java8/operations/Java8Operations.java
@@ -39,7 +39,7 @@ public class Java8Operations {
 	}
 
 	public static void main(String[] args) {
-		
+
 //		Date Time API in java-8
 //		1. To get current Date
 //			java.time.LocalDate.now()
@@ -52,7 +52,7 @@ public class Java8Operations {
 		System.out.println("getAverageSalaryByGender: " + getAverageSalaryByGender(emps));
 		System.out.println("getAverageAgeByGender: " + getAverageAgeByGender(emps));
 		System.out.println("countEmployeesWithSalaryGreaterThan100000: " + countEmployeesWithSalaryGreaterThan100000(emps));
-		System.out.println(sumOfAllSalaries(emps));
+		System.out.println("sumOfAllSalaries: " + sumOfAllSalaries(emps));
 		System.out.println("OldestEmployee: " + OldestEmployee(emps));
 		System.out.println(employeesGroupedByDepartment(emps));
 		System.out.println("countOfEmployeesInEachDepartment : " + countOfEmployeesInEachDepartment(emps));
@@ -86,44 +86,44 @@ public class Java8Operations {
 				.collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
 		return map;
 	}
-	
+
 	private static Employee getEmployeeWithHighestSalary(List<Employee> emps) {
 		Optional<Employee> emp = emps.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary)));
 //		Employee emp = emps.stream().reduce((a, b) -> a.getSalary() > b.getSalary()? a: b).get();
 //		Employee emp = emps.stream().max((emp1, emp2) -> (int)emp1.getSalary() - (int)emp2.getSalary()).get();
 		return emp.isPresent()? emp.get(): null;
 	}
-	
+
 	private static Employee getFemaleEmployeeWithHighestSalary(List<Employee> emps) {
 		Employee emp = emps.stream().filter(e -> e.getGender() == 'F').collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary))).get();
 		return emp;
 	}
-	
+
 	static private Map<Character, Double> getAverageSalaryByGender(List<Employee> emps){
 //		return emps.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getSalary)));
 		return emps.stream().collect(Collectors.groupingBy(e -> e.getGender(), Collectors.averagingDouble(e -> e.getSalary())));
 	}
-	
+
 	static private Map<Character, Double> getAverageAgeByGender(List<Employee> emps){
 		return emps.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
 	}
-	
+
 	static private long countEmployeesWithSalaryGreaterThan100000(List<Employee> emps) {
 		return emps.stream().filter(e -> e.getSalary() > 100000).count();
 	}
-	
+
 	static private double sumOfAllSalaries(List<Employee> emps) {
-//		return emps.stream().mapToDouble(e -> e.getSalary()).sum();
-		return emps.stream().mapToDouble(x -> x.getSalary()).sum();
+		return emps.stream().mapToDouble(x -> x.getSalary()).reduce((x, b) -> x + b).getAsDouble();
+//		return emps.stream().mapToDouble(x -> x.getSalary()).sum();
 	}
-	
+
 	static private Employee OldestEmployee(List<Employee> emps) {
 //		return (Employee) emps.stream().sorted(Comparator.comparingInt(Employee::getAge)).limit(1);
 //		return emps.stream().reduce((e1, e2) -> e2.getAge() > e1.getAge()? e2: e1).get();
 		return (Employee) emps.stream().collect(Collectors.maxBy(Comparator.comparingInt(Employee::getAge))).get();
 //		return emps.stream().max((x1, x2) -> x1.getAge() - x2.getAge()).get();
 	}
-	
+
 	static private Map<String, List<Employee>> employeesGroupedByDepartment(List<Employee> emps){
 //		If you want only emp id/name instead of whole emp obj do this
 		Map<String, List<Integer>> map = emps.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.mapping(Employee::getId, Collectors.toList())));
@@ -131,33 +131,39 @@ public class Java8Operations {
 //		System.out.println(map);
 		return emps.stream().collect(Collectors.groupingBy(Employee::getDepartment));
 	}
-	
+
 	static private Map<String, Long> countOfEmployeesInEachDepartment(List<Employee> emps){
 		return emps.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
 	}
-	
+
 	static private Map<String, Double> averageSalaryOfEachDepartment(List<Employee> emps){
 		return emps.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(emp -> emp.getSalary())));
 	}
-	
+
 	static private Map<String, Optional<Employee>> highestSalaryInEachDepartment(List<Employee> emps){
 		return emps.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary))));
 	}
-	
+
 	static private Entry<Double, List<Employee>> findNthHighestSalary(List<Employee> emps, int n){
 //		first group emps by their salaries, becuase there mayt be emps with same salaries.
 //		below map stores all the unique salaries with list of emps;
 		Map<Double, List<String>> map = emps.stream().collect(Collectors.groupingBy(Employee::getSalary, Collectors.mapping(Employee::getFirstName, Collectors.toList())));
 		List<Entry<Double, List<String>>> collect = map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByKey())).collect(Collectors.toList());
 //		System.out.println(collect);
+//		return emps.stream().collect(Collectors.groupingBy(e -> e.getSalary(), Collectors.toList()))
+//				.entrySet().stream()
+//				.sorted((a, b) -> (int)(b.getKey() - a.getKey()))
+//				.skip(n-1)
+//				.limit(1).iterator().next();
+
 		return emps.stream().collect(Collectors.groupingBy(e -> e.getSalary(), Collectors.toList()))
 				.entrySet().stream()
 				.sorted((a, b) -> (int)(b.getKey() - a.getKey()))
 				.skip(n-1)
-				.limit(1).iterator().next();
+				.findFirst().get();
 //		return collect.get(n-1);
 	}
-	
+
 	static private Map<Character, Long> countOfEachCharacter(String s){
 //		codePoints() returns ascii values of each character as intStream.
 //		return s.codePoints().mapToObj(x -> (char)x).collect(Collectors.groupingBy(x->x, Collectors.counting()));
@@ -166,7 +172,7 @@ public class Java8Operations {
 //		Other way by using Function.identity() which identifies unique characters.
 		return s.codePoints().mapToObj(x -> (char)x).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 	}
-	
+
 	static private List<Character> findAllDuplicateCharactersInAGivenString(String s) {
 //		Other short way
 //		return s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, Collectors.counting()))
@@ -174,57 +180,57 @@ public class Java8Operations {
 //		.filter(x -> x.getValue() > 1)
 //		.map(Map.Entry::getKey)
 //		.collect(Collectors.toList());
-		
+
 		Map<Character, Long> collect = s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 		Set<Character> set = collect.keySet();
 		Stream<Character> stream = set.stream().filter(x -> collect.get(x) > 1);
 		return stream.collect(Collectors.toList());
 
 	}
-	
+
 	static private List<Character> findAllUniqueCharactersInAGivenString(String s) {
 //		Q. Find if all the chars in string are unique:
 //		Set<Character> set = new HashSet();
 //		s.codePoints().mapToObj(x -> (char)x).collect(Collectors.toSet());
 //		boolean result = set.size() == s.length();
-		
-		
+
+
 //		Other short way
 		return s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, Collectors.counting()))
 		.entrySet().stream()
 		.filter(x -> x.getValue() == 1)
 		.map(Map.Entry::getKey)
 		.collect(Collectors.toList());
-		
+
 //		Map<Character, Long> collect = s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 //		Set<Character> set = collect.keySet();
 //		Stream<Character> stream = set.stream().filter(x -> collect.get(x) == 1);
 //		return stream.collect(Collectors.toList());
-		
+
 
 	}
-	
+
 	static private char findFirstNonRepetitiveCharacterInAGivenString(String s) {
 //		Other short way
 //		return s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()))
 //		.entrySet().stream()
 //		.filter(x -> x.getValue() == 1)
 //		.findFirst().get().getKey();
-		
+
 //		LinkedHashMap::new -> by doing this we say stream not to use hashmap but use linkedHashMap 
 		Map<Character, Long> collect = s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()));
 		Set<Character> set = collect.keySet();
 		Stream<Character> stream = set.stream().filter(x -> collect.get(x) == 1);
 		return stream.findFirst().get();
 	}
-	
+
 	static private char findFirstRepetitiveCharacterInAGivenString(String s) {
 //		Other short way
 //		return s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, Collectors.counting()))
 //		.entrySet().stream()
 //		.filter(x -> x.getValue() > 1)
 //		.findFirst().get().getKey();
-		
+
 //		LinkedHashMap::new -> by doing this we say stream not to use hashmap but use linkedHashMap 
 		Map<Character, Long> collect = s.codePoints().mapToObj(c  -> (char)c).collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()));
 		Set<Character> set = collect.keySet();
@@ -232,11 +238,11 @@ public class Java8Operations {
 		return stream.findFirst().get();
 
 	}
-	
+
 	static private int findSecondLowestNumberInAGivenArray(int[] arr) {
 		return Arrays.stream(arr).sorted().skip(1).limit(1).toArray()[0];
 	}
-	
+
 	static private int findSecondHighestNumberInAGivenArray(int[] arr) {
 		int[] array = Arrays.stream(arr).sorted().toArray();
 //		Other simple way
@@ -249,27 +255,27 @@ public class Java8Operations {
 //		if array has duplicates
 		return (int)Arrays.stream(arr).boxed().distinct().sorted(Comparator.reverseOrder()).skip(1).limit(1).toArray()[0];
 	}
-	
+
 	private static String findStringWithGreatestLength(String[] strArr) {
 //		return Arrays.stream(strArr).reduce((s1, s2) -> s2.length() > s1.length()? s2: s1).get();
 //		if input array has elements with same length:
 		return Arrays.stream(strArr).collect(Collectors.groupingBy(String::length, Collectors.toList())).entrySet()
 				.stream().sorted(Collections.reverseOrder(Map.Entry.comparingByKey())).collect(Collectors.toList()).get(0).getValue().get(0);
 	}
-	
+
 	private static List<String> findAllElementsWhichStartsWith1InAnArray(int[] arr) {
 		return Arrays.stream(arr).boxed().map(s -> s+"").filter(s -> s.startsWith("1")).collect(Collectors.toList());
 	}
-	
+
 	private static List<Integer> findEvenInAList(int[] arr){
 		return Arrays.stream(arr).boxed().filter(x -> x%2 == 0).collect(Collectors.toList());
 	}
-	
+
 	private static String stringJoinExample(List<String> list) {
 //		String.join(delimeter, list) -> u can use any delimeter
 		return String.join("-", list);
 	}
-	
+
 	private static List<Integer> skipFirstAndLastInAList(IntStream stream) {
 //		Other way
 //		IntStream.rangeClosed(1, 10)
@@ -278,14 +284,14 @@ public class Java8Operations {
 //		.forEach(System.out::print);
 		return stream.mapToObj(x->(int)x).skip(1).limit(8).collect(Collectors.toList());
 	}
-	
+
 	private static List<Integer> findAllDuplicateElementsInAGivenArray(int[] arr){
 		return Arrays.stream(arr).boxed() // converting given array to stream
 				.collect(Collectors.groupingBy(x->x, Collectors.counting())) // grouping by their frequency
 				.entrySet().stream()  // above gives us a map(key-> element, value-> key's frequency), so convert it to stream
 				.filter(x -> x.getValue() > 1).map(x -> x.getKey()) // now get whichever element has frequency more than 1.
 				.collect(Collectors.toList()); // finally collecting.
-		
+
 		// Other simple way
 		/*
 		 * Set<Integer> set = new HashSet<>();
@@ -294,39 +300,39 @@ public class Java8Operations {
 			.forEach(System.out::println); */
 //		return null;
 	}
-	
+
 	private static Optional<String> findFirstElementInList(List<String> list) {
 		Optional<String> out = list.stream().findFirst();
 		return out;
 	}
-	
+
 	private static long findCountOfElements(int[] arr){
 		return Arrays.stream(arr)
-				.count(); 
+				.count();
 	}
-	
+
 	private static long findMaxInAList(List<Integer> list){
 		Optional<Integer> opt = list.stream()
 				.max(Integer::compare);
 		return opt.isPresent()? opt.get(): 0;
 	}
-	
+
 	private static List<String> concatenateTwoStreams(List<String> l1, List<String> l2) {
 		// Concatenated the list1 and list2 by converting them into Stream
 		Stream<String> concatStream = Stream.concat(l1.stream(), l2.stream());
-         
+
 		// Printed the Concatenated Stream
 //        concatStream.forEach(str -> System.out.print(str + " "));
-		
+
 		List<String> collect = concatStream.collect(Collectors.toList());
 		return collect;
-        
+
 	}
-	
+
 	private static Map<String, Long> findOnlyDuplicateStringsWithItsCountFromAList(List<String> l1){
-		
+
 		IntStream.rangeClosed(1, 10).mapToObj(x -> x).collect(Collectors.toList());
-		
+
 		return l1.stream()
 				.filter(x -> Collections.frequency(l1, x) > 1)
 				.collect(Collectors.groupingBy
@@ -337,7 +343,7 @@ public class Java8Operations {
 //				.filter(x -> x.getValue() > 1)
 //				.collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
 	}
-	
+
 }
 
 
